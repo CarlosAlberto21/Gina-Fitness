fbf71d6a50d62fe59d0fe1635e2a839cac2f0bd2
CarlosAlberto21 <75591039+CarlosAlberto21@users.noreply.github.com>
2025-05-01 11:39:59 -0400

    Actualización del proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gina-fitness/', // <--- importante
   plugins: [react(), tailwindcss()],
 })
